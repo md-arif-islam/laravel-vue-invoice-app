b0ac8b6ce35d79b830b658abff835dfeff980c91
md-arif-islam <arifislamarif344@gmail.com>
2023-08-30 15:45:59 +0600

    UPDATED: Fixed typo in "NotFound" component and path in router.
    - Fixed typo in "Not FOund" component, changed to "Not Found".
    - Updated path in router from "/pathMatch(.*)*" to "/:pathMatch(.*)*" for the NotFound component.

--- resources/js/components/NotFound.vue
+++ resources/js/components/NotFound.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <h1>Not FOund</h1>
+        <h1>Not Found</h1>
     </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style></style>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -8,7 +8,7 @@ const routes = [
         component: Invoice,
     },
     {
-        path: "/pathMatch(.*)*",
+        path: "/:pathMatch(.*)*",
         component: NotFound,
     },
 ];
